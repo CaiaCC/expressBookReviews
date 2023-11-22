2b166e8f632dab1400b13d16f846c677bc747c4f
Caia Chuang <caiachuang@gmail.com>
2023-11-21 18:18:35 -0800

    add registeration mechanism

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,20 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { username, password } = req.body;
+
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({ username, password });
+      return res.status(200).json({ message: " Registration successful! Please log in." });
+    }
+
+    return res.status(404).json({ message: " User already exists." });
+  }
+
+  return res.status(404).json({ message: "Unable to complete register. Username and password must be provided." });
 });
+
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
