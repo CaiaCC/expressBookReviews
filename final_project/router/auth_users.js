a913b6c8c30913ec1cdacccaee0ef04382cc081b
Caia Chuang <caiachuang@gmail.com>
2023-11-21 19:10:54 -0800

    delete review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -56,6 +56,21 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({ message: "Review added!" });
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const { isbn } = req.params;
+  const { username } = req.session.authorization;
+  if (!username) return res.status(403).json({ message: "Must log in to delete a review." });
+
+  if (!books[isbn].reviews[username]) {
+    return res.status(404).json({ message: "Review not existed!" });
+  }
+
+  delete books[isbn].reviews.username;
+
+  return res.status(200).json({ message: "Review deleted!" });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
